f3aef3e6640b11524a346092cbeed18a3682a33f
trangle972501 <trangle972501@gmail.com>
2017-07-12 12:50:47 +0700

    reponsive slide

--- resources/views/frontend/components/header.blade.php
+++ resources/views/frontend/components/header.blade.php
@@ -1,22 +1,17 @@
-<div class="header">
-	<div class="row">
-		<div class="col-md-3 col-xs-3 col-sm-3 il-logo">
-			<a href="" title=""><img src="{{ asset('img/logo.png')}}" alt="il-logo" class="il-logo-md"></a>
-		</div>
-		<div class="col-md-9 col-xs-9 col-sm-3 il-header-right">
-			<div class="row il-header-right-top">
-				<a href="">Đăng nhập  |</a>
-				<a href="">Đăng ký</a>
-			</div>
-			<div class="clearfix"></div>
-			<div class="row il-header-right-banner">
-				<!-- <div id="slides">
-					<div class="slide showing">Hệ thống tra từ ILEARN</div>
-					<div class="slide">3 ngôn ngữ: Anh-Việt-Nhật</div>
-					<div class="slide">Đơn giản, dễ sử dụng</div>
-				</div> -->
-				<!-- <img src="{{ asset('img/slide.png')}}" alt=""> -->
-			</div>
-		</div>
+
+<div class="col-md-3 col-xs-3 col-sm-3 il-logo">
+	<a href="" title=""><img src="{{ asset('img/logo.png')}}" alt="il-logo" class="il-logo-md"></a>
+</div>
+<div class="col-md-9 col-xs-9 col-sm-9 il-header">
+	<div class="col-md-12 il-header-right-top">
+		<a href="">Đăng nhập  |</a>
+		<a href="">Đăng ký</a>
+	</div>
+	<div class="clearfix"></div>
+	<div class="col-md-12 il-header-right-banner" id="slides">
+		<div class="col-md-12 slide showing image-responsive">Hệ thống tra từ ILEARN</div>
+		<div class="col-md-12 slide image-responsive">3 ngôn ngữ: Anh-Việt-Nhật</div>
+		<div class="col-md-12 slide image-responsive">Đơn giản, dễ sử dụng</div>
 	</div>
 </div>
+
