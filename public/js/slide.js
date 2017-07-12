36b9c0bc70898874c03fde0b5d0cad760d2595f5
trangle972501 <trangle972501@gmail.com>
2017-07-12 10:14:13 +0700

    responsive header

--- public/js/slide.js
+++ public/js/slide.js
@@ -1,10 +1,14 @@
+$(document). ready(function(){
 
-var slides = document.querySelectorAll('#slides .slide');
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,2000);
+	var slides = $(document).find('.slide');
+	var currentSlide = 0;
+	var timeSlide = 2000;
+	var slideInterval = setInterval(nextSlide,timeSlide);
 
-function nextSlide(){
-	slides[currentSlide].className = 'slide';
-	currentSlide = (currentSlide+1)%slides.length;
-	slides[currentSlide].className = 'slide showing';
-}
+	function nextSlide(){
+		slides[currentSlide].className = 'slide';
+		currentSlide = (currentSlide+1)%slides.length;
+		slides[currentSlide].className = 'slide showing';
+	}
+
+});
