8ef69561d31f1aa988f0f46d6900a79ae550b583
SCORPION <nguyenthanhat011195@gmail.com>
2017-07-20 16:16:24 +0700

    Update Crawler

--- routes/web.php
+++ routes/web.php
@@ -73,6 +73,10 @@ Route::group(['prefix' => 'admin'], function () {
     Route::GET('search','DictionaryManagementController@search')->name('adminSearch');
     Route::GET('upload','DictionaryManagementController@upload')->name('adminUpload');
     Route::POST('postUpload', 'AdminCrawlerController@postUploadWords')->name('adminPostUpload');
+
+    Route::GET('testPutSession', 'AdminCrawlerController@testPutSession');
+    Route::GET('testGetSession', 'AdminCrawlerController@testGetSession');
+    Route::GET('testEndSession', 'AdminCrawlerController@testEndSession');
     });
 });
 // END ADMIN
