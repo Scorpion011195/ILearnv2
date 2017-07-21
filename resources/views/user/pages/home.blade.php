ff0b575c0f71df50fe8dc8fb15517520461fe1da
trangle972501 <trangle972501@gmail.com>
2017-07-21 11:03:34 +0700

    changes

--- resources/views/user/pages/home.blade.php
+++ resources/views/user/pages/home.blade.php
@@ -1,9 +1,11 @@
 @extends('user.layouts.index')
 @section('content')
 @include('user.components.search.search')
-    @if($status = Session::get('notify'))
-        <div class="alert alert-info" role="alert">
-            {{ $status}}
-        </div>
-    @endif()
+	<div class="col-lg-6 col-md-8 col-sm-8 col-xs-12 col-lg-offset-3 col-md-offset-2 col-sm-offset-2">
+	    @if($status = Session::get('status'))
+	        <div class="alert alert-info" role="alert">
+	            {{ $status}}
+	        </div>
+	    @endif()
+	</div>
 @endsection
