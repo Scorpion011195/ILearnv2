61604a34523353da5077e95fe975f2bce23f0da0
SCORPION <nguyenthanhat011195@gmail.com>
2017-07-24 09:06:25 +0700

    no message

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,40 +11,41 @@
 */
 /*=================USER AREA==================*/
 Route::get('/', function () {
-	return view('welcome');
+    return view('welcome');
 });
-
 Route::get('login', [ 'as' => 'login', 'uses' => 'UserController@getLogin']);
 Route::post('login', [ 'as' => 'login', 'uses' => 'UserController@postLogin']);
-
 Route::get('register', [ 'as' => 'register', 'uses' => 'UserController@getRegister']);
 Route::post('register', [ 'as' => 'register', 'uses' => 'UserController@postRegister']);
 Route::get('logout', ['as'=>'logout', 'uses' => 'UserController@logout']);
-
 Route::get('user/pages/verifyEmail/{confirmationCode}','UserController@confirm'
 )->name('confirm');
 
-Route::get('index', function(){
-	return view('user/layouts/index');
-});
+// Route::get('index', function(){
+//  return view('user/layouts/index');
+// });
 
-Route::get('home', function(){
-    return view('user/pages/home');
-});
+// Link for User search Word
+Route::get('home', ['as' => 'home', 'uses' => 'DictionaryController@getSearchDictionary']);
+Route::get('search', ['as' => 'search', 'uses' => 'DictionaryController@postSearchDictionary']);
 
+Route::get('notify', function(){
+    return view('user/pages/notify');
+});
 Route::get('result', function(){
     return view('user/pages/result');
 });
 
+Route::get('history', function(){
+    return view('user/pages/history');
+});
+
 Route::get('translate', 'TranslateController@getTranslateParagraph');
-
 Route::get('translate-paragraph', ['as' => 'translateParagraph', 'uses' => 'TranslateController@translateParagraph']);
-
 // Route::get('profile', ['as' => 'profile', 'uses'=>'UserController@getShowUser']);
 Route::get('profile', function(){
     return view('user/pages/profile');
 })->middleware('auth');
-
 Route::get('editprofile/{id}', ['as' => 'editprofile/{id}', 'uses' => 'UserController@getEditUser'])->middleware('auth');
 Route::post('editprofile/{id}', ['as' => 'editprofile/{id}', 'uses' => 'UserController@postEditUser'])->middleware('auth');
 //User change password
@@ -53,6 +53,7 @@ Route::get('changePass', ['as' => 'changePass', 'uses' => 'UserController@getCha
 Route::post('changePass', ['as' => 'changePass', 'uses' => 'UserController@postChangePass'])->middleware('auth');
 
 //Language My Word
+
 Route::post('myWord','WordUserController@addWordFromSearch');
 
 /*=================ADMIN AREA==================*/
@@ -60,72 +61,19 @@ Route::group(['prefix' => 'admin'], function () {
     // Đăng nhập
     Route::get('login', 'AdminController@getLogin')->name('adminGetLogin');
     Route::post('login', 'AdminController@postLogin')->name('adminPostLogin');
-
     // Đăng xuất
     Route::get('logout', 'AdminController@logout')->name('adminLogout');
-
     // Trang chủ
     Route::group(['middleware' =>'AdminLogin'],function(){
-   	Route::get('/',function(){
-   		return view('admin.layouts.ilearn');
-   	});
-    //  add word
-    Route::GET('get', 'DictionaryManagementController@home')->name('getAddWord');
-    Route::POST('add', 'DictionaryManagementController@getAddWord')->name('adminAdd');
-    Route::GET('search','DictionaryManagementController@search')->name('adminSearch');
-    Route::GET('upload','DictionaryManagementController@upload')->name('adminUpload');
-    Route::POST('postUpload', 'AdminCrawlerController@postUploadWords')->name('adminPostUpload');
-
-        /*Quản lý từ điển*/
-        Route::group(['prefix' => 'dict','middleware'=>'AdminLogin'], function () {
-        //  add word
-            Route::GET('get', 'DictionaryManagementController@home')->name('getAddWord');
-            Route::POST('add', 'DictionaryManagementController@getAddWord')->name('adminAdd');
-        // Search word
-            Route::GET('search','DictionaryManagementController@getSearch')->name('adminDisplay');
-            Route::GET('search/result','DictionaryManagementController@search')->name('adminSearch');
-        // Delete Word
-            Route::post('delete', 'DictionaryManagementController@deleteWord');
-        // Update từ
-            Route::post('update', 'DictionaryManagementController@updateWord');
-        // Upload file
-            Route::GET('upload','DictionaryManagementController@upload')->name('adminUpload');
-        });
-        // Thông tin cá nhân
-        Route::group(['prefix' => 'profile','middleware'=>'AdminLogin'], function () {
-            Route::get('/', 'AdminController@getProfile')->name('adminProfile');
-
-            Route::post('/', 'AdminController@updateProfile')->name('updateProfile');
-        });
-        /*Quản lý user*/
-        Route::group(['prefix' => 'account','middleware'=>'AdminLogin'], function () {
-        // Admin Seach user
-            Route::get('get', 'UserManagementController@getAccount')->name('adminUserManagement');
-            Route::get('search', 'UserManagementController@searchUser')->name('adminSearchUser');
-
-             Route::post('status', 'UserManagementController@changeStatus');
-             Route::post('role', 'UserManagementController@changeRole');
-
-             Route::post('deleteUser', 'UserManagementController@deleteUser');
-
-            // Route::get('detail/{id}', 'UserManagementController@getDetailUser')->name('adminGetDetailUser');
-            // Route::post('updateDetail', 'UserManagementController@postDetailUser')->name('adminPostDetailUser');
-
-
+    Route::get('/',function(){
+        return view('admin.layouts.ilearn');
     });
-
     //  add word
     Route::GET('get', 'DictionaryManagementController@home')->name('getAddWord');
     Route::POST('add', 'DictionaryManagementController@getAddWord')->name('adminAdd');
     Route::GET('search','DictionaryManagementController@search')->name('adminSearch');
     Route::GET('upload','DictionaryManagementController@upload')->name('adminUpload');
     Route::POST('postUpload', 'AdminCrawlerController@postUploadWords')->name('adminPostUpload');
-    Route::GET('testPutSession', 'AdminCrawlerController@testPutSession');
-    Route::GET('testGetSession', 'AdminCrawlerController@testGetSession');
-    Route::GET('testEndSession', 'AdminCrawlerController@testEndSession');
-
     });
 });
-
 // END ADMIN
-
