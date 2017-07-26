2b2262e35b889d4a4d8fac03d6574ed4808846c0
SCORPION <nguyenthanhat011195@gmail.com>
2017-07-26 09:34:28 +0700

    no message

--- public/js/admin/admin.js
+++ public/js/admin/admin.js
@@ -207,15 +207,5 @@ $(document).ready(function() {
 
     /*Pop hover*/
     $('[data-toggle="popover"]').popover();
-
-    // Upload dictionarys
-    $(document).on('submit', '#form_upload', function(evt){
-        $('.btn_upload').prop('disabled', true);
-
-        var alertWaiting = '<div class="loader"></div>';
-        $('.alert_waiting').replaceWith(alertWaiting);
-
-        return true;
-    });
 });
 
