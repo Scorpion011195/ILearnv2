ffee9342b671956aee6846c3264cb94b8a7e941c
trangle972501 <trangle972501@gmail.com>
2017-08-03 11:23:42 +0700

    update UI for Ilearn

--- resources/views/user/pages/verifyEmail.blade.php
+++ resources/views/user/pages/verifyEmail.blade.php
@@ -1,10 +1,7 @@
-@extends('user.layouts.index')
-@section('content')
-	<h2>Verify Your Email Address</h2>
-	Hi {!! $username !!}
-        <div>
-            Thanks for creating an account with the verification demo app.
-            Please follow the link below to verify your email address
-            {{ route ('confirm', $confirmation_code)}}
-        </div>
-@endsection
+<h2>XÁC THỰC TÀI KHOẢN</h2>
+	Chào {!! $username !!},<b>
+    <div>
+        Bạn vừa gửi yêu cầu xác thực email trên <strong>ILEARN</strong><b>.
+        Vui lòng nhấn vào liên kết bên dưới để hoàn thành quá trình đăng kí.<b>
+        {{ route ('confirm', $confirmation_code)}}
+    </div>
